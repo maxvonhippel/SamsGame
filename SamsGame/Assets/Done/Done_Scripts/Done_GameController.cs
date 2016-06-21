18918be84f607aafebbf10f323032f2157080f8d
maxvonhippel <maxvonhippel1996@gmail.com>
2016-06-20 22:30:06 -0700

    OH SHIT HOMIES WE GOT ALMOST ALL THE SAM MADE GRAPHICS WORKING ... I mean we got Sam's drawings hooked up so everything looks pretty legit, though we still need to replace the commets/astroids, add traps and gems and money, and make the backgrounds not stretch, and test the background switch timer, and make the transitions smoother and.. well there's still a lot to do.

--- SamsGame/Assets/Done/Done_Scripts/Done_GameController.cs
+++ SamsGame/Assets/Done/Done_Scripts/Done_GameController.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
+using System.Timers;
 
 [System.Serializable]
 public class MutantMachine : System.Object {
@@ -17,8 +19,15 @@ public class Done_GameController : MonoBehaviour
 {
 	// array of mutant machines
 	public MutantMachine[] machines;
-
+	// array of hazards of all types
 	public GameObject[] hazards;
+	// array of backgrounds for the game
+	public Texture[] backgrounds;
+	public GameObject curBackground;
+	private int nBackground;
+
+	public GameObject graphicsPrefab;
+
 	public Vector3 spawnValues;
 	public int hazardCount;
 	public float spawnWait;
@@ -42,16 +51,47 @@ public class Done_GameController : MonoBehaviour
 		score = 0;
 		UpdateScore ();
 		StartCoroutine (SpawnWaves ());
+
+		System.Timers.Timer aTimer = new System.Timers.Timer();
+		aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+		// every 10 seconds
+		aTimer.Interval = 10000;
+		aTimer.Enabled = true;
+
+		nBackground = 0;
+		curBackground.GetComponent<MeshRenderer> ().material.mainTexture = backgrounds[nBackground];
+	}
+
+	private void OnTimedEvent(object source, ElapsedEventArgs e)
+	{
+		if (backgrounds.Length == 0)
+			return;
+		nBackground += 1;
+		if (nBackground > backgrounds.Length)
+			nBackground = 0;
+		// now change the background
+		curBackground.GetComponent<MeshRenderer> ().material.mainTexture = backgrounds[nBackground];
+		return;
 	}
 	
 	void Update ()
 	{
-		if (restart)
+		if (restart && Input.GetKeyDown (KeyCode.R))
+			Application.LoadLevel (Application.loadedLevel);
+	}
+
+	void SetLayerRecursively(GameObject obj, int newLayer)
+	{
+		if (null == obj)
+			return;
+
+		obj.layer = newLayer;
+
+		foreach (Transform child in obj.transform)
 		{
-			if (Input.GetKeyDown (KeyCode.R))
-			{
-				Application.LoadLevel (Application.loadedLevel);
-			}
+			if (null == child)
+				continue;
+			SetLayerRecursively(child.gameObject, newLayer);
 		}
 	}
 	
@@ -62,10 +102,29 @@ public class Done_GameController : MonoBehaviour
 		{
 			for (int i = 0; i < hazardCount; i++)
 			{
-				GameObject hazard = hazards [Random.Range (0, hazards.Length)];
-				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+				int rando = UnityEngine.Random.Range (0, hazards.Length);
+
+				Vector3 spawnPosition = new Vector3 (UnityEngine.Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
 				Quaternion spawnRotation = Quaternion.identity;
-				Instantiate (hazard, spawnPosition, spawnRotation);
+				GameObject hazard = Instantiate (hazards [rando], spawnPosition, spawnRotation) as GameObject;
+
+				// is it an enemy ship?
+				if (rando == hazards.Length - 1) {
+					// make the ship invisible
+					hazard.layer = 4;
+					SetLayerRecursively (hazard, 4);
+					// first off, randomly select a mutant machine
+					MutantMachine curMachine = machines [ UnityEngine.Random.Range (0, machines.Length)];
+					// next, make the hazard have a picture of the mutant machine
+					GameObject graphic = Instantiate (graphicsPrefab, spawnPosition, spawnRotation) as GameObject;
+					graphic.transform.Rotate(0, 180, 0);
+					graphic.GetComponent<MeshRenderer> ().material.mainTexture = curMachine.graphicTexture;
+					graphic.transform.parent = hazard.transform;
+
+				} else {
+					// assign a comet or other such random item to the picture
+
+				}
 				yield return new WaitForSeconds (spawnWait);
 			}
 			yield return new WaitForSeconds (waveWait);
